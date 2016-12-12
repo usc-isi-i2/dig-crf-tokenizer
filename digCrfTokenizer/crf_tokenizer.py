27f83478274c2a58947f554b97e5991740949278
Pedro Szekely <szekely1401@gmail.com>
2016-12-11 17:13:41 -0800

    convert to set literal

--- digCrfTokenizer/crf_tokenizer.py
+++ digCrfTokenizer/crf_tokenizer.py
@@ -73,11 +73,11 @@ result = t.tokenize(value).join(" ")
 
     whitespaceSet = set(string.whitespace)
     punctuationSet = set(string.punctuation)
-    htmlEntityNameCharacterSet = set(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-                                      'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-                                      'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-                                      'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-                                      '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '#'])
+    htmlEntityNameCharacterSet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                                  'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                                  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                                  'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+                                  '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '#'}
     START_HTML_TAG_CHAR = "<"
     END_HTML_TAG_CHAR = ">"
     START_HTML_ENTITY_CHAR = "&"
